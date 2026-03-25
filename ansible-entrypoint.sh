a1a31f9e40dbafa3a1400182b5ea19ed888c2c5f
user <user@mail.net>
2026-03-25 15:10:44 +0300

    В переменной окружения EXECUTABLE можно задать исполняемый файл

--- ansible-entrypoint.sh
+++ ansible-entrypoint.sh
@@ -41,7 +41,7 @@ if [[ -n "$gid" ]]; then
 fi
 
 if [[ "${1:0:1}" = '-' ]]; then
-  set -- ansible-playbook "$@"
+  set -- "${EXECUTABLE:-ansible-playbook}" "$@"
 fi
 
 if [[ -z "$uid" && -z "$gid" ]]; then
